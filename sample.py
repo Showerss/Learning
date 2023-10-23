f0d8dc5cfd20c8553e90fccecddf2bb93163b9f9
PBanky <pbanky@sandiego.edu>
2023-10-23 14:02:40 -0700

    learning

--- sample.py
+++ sample.py
@@ -4,123 +4,77 @@ import random
 
 phil = turtle.Turtle()
 
-# def project():
-#     phil.forward(20)
-#     phil.left(90)
-#     phil.forward(20)
-#     phil.right(90)
-#     phil.forward(20)
+def moveRight():
+    global direction
+    direction = 'right'
 
-#     turtle.exitonclick()
+def moveLeft():
+    global direction
+    direction = 'left'
 
-# project()
+def moveDown():
+    global direction
+    direction = 'down'
 
+def moveUp():
+    global direction
+    direction = 'up'
 
-# def newthing():
-#     phil.shape('circle')
-
-#     phil.setx(phil.xcor() + 20)
-#     time.sleep(0.5)
-
-#     phil.sety(phil.ycor() + 20)
-#     time.sleep(0.5)
-
-#     phil.setx(phil.xcor() + 20)
-#     time.sleep(0.5)
-
-#     phil.sety(phil.ycor() + 20)
-
-#     turtle.exitonclick()
-
-# newthing()
+turtle.listen()
+turtle.onkeypress(moveUp, 'Up')
+turtle.onkeypress(moveDown, 'Down')
+turtle.onkeypress(moveRight, 'Right')
+turtle.onkeypress(moveLeft, 'Left')
 
 def spawnFood():
     screen = turtle.Screen()
     ww = (screen.window_width() // 2) // 20 
     wh = (screen.window_height() // 2) // 20 
-    food_x = random.randrange(-ww, ww) *20 
-    food_y = random.randrange(-ww, ww) *20 
+    food_x = random.randrange(-ww, ww) * 20 
+    food_y = random.randrange(-ww, ww) * 20 
     food = turtle.Turtle('triangle')
     food.color('yellow')
     food.penup()
     food.goto(food_x, food_y)
 
-def thirdthing():
-    philbody = [turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle()] #create 4 turtles
-    turtlecolors = ['black', 'red', 'blue', 'green']
+# def thirdthing():
+philbody = [turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle()] #create 4 turtles
+turtlecolors = ['black', 'red', 'blue', 'green']
 
-    for i in range(len(philbody)): #will get the length of the list and return 4 to count 0,1,2,3
-        philbody[i].speed('slowest')
-        philbody[i].penup()
-        philbody[i].shape('circle') #for each part of philbody make each new one the circle
-        philbody[i].color(turtlecolors[i]) #for each one in philbody, phil and etc etc
-        philbody[i].setx(i * (20)) #move the x coordinates to 1*-20, 2*-20, etc
+for i in range(len(philbody)): #will get the length of the list and return 4 to count 0,1,2,3
+    philbody[i].speed('fastest')
+    philbody[i].penup()
+    philbody[i].shape('circle') #for each part of philbody make each new one the circle
+    philbody[i].color(turtlecolors[i]) #for each one in philbody, phil and etc etc
+    philbody[i].setx(i * (20)) #move the x coordinates to 1*-20, 2*-20, etc
 
-    def moveRight():
-        move('right')
+direction = ''
+spawnFood()
 
-    def moveLeft():
-        move('left')
+def move(direction):
+    coords = 0,0
+    if direction == 'left':
+        coords = philbody[0].xcor() - 20, philbody[0].ycor()
+    elif direction == 'right':
+        coords = philbody[0].xcor() + 20, philbody[0].ycor()
+    elif direction == 'up':
+        coords = philbody[0].xcor(), philbody[0].ycor() + 20
+    elif direction == 'down':
+        coords = philbody[0].xcor(), philbody[0].ycor() - 20
 
-    def moveDown():
-        move('down')
+    philtail = philbody.pop() #pop the end off and store it to the variable philtail
+    philbody.insert(0,philtail) #insert philtail to position 0 of philbody
+    philtail.goto(*coords) #reset the end to the beginning of the bodys position * 20 in x value ...the *coords recognizes the comma above and uses them as two seperate values to pass throgh coords
+    
+while True:
+    move(direction)
 
-    def moveup():
-        move('up')
-
-    spawnFood()
-
-    def move(direction):
-        coords = 0,0
-        if direction == 'left':
-            coords = philbody[0].xcor() - 20, philbody[0].ycor()
-        elif direction == 'right':
-            coords = philbody[0].xcor() + 20, philbody[0].ycor()
-        elif direction == 'up':
-            coords = philbody[0].xcor(), philbody[0].ycor() +20
-        elif direction == 'down':
-            coords = philbody[0].xcor(), philbody[0].ycor() -20
-
-        philtail = philbody.pop() #pop the end off and store it to the variable philtail
-        philbody.insert(0,philtail) #insert philtail to position 0 of philbody
-        philtail.goto(*coords) #reset the end to the beginning of the bodys position * 20 in x value
-        # the *coords recognizes the comma above and uses them as two seperate values to pass throgh coords
-        
-    move('right')
-    move('down')
-    move('right')
-    move('up')
-    move('up')
-    move('left')
-
-
-
+    #dictionary
+    snake_body = {snake_body[0]:philbody[0], 
+            snake_body[1]:philbody[1], 
+            snake_body[2]:philbody[2], 
+            snake_body[3]:philbody[3]}
 
     turtle.exitonclick()
-thirdthing()
-
-
-
-''' 
-in order: 
-
-before game starts: (globals)
-spawn food
-
-while in game:
-    move
-    if snake head collides into food
-        spawn more food
-
-    ========
-
-how do we check for colisions? 
-what does that look like in code
-
-    ========
-
-module... import random
-
-'''
 
 
